4ffc2d0a2b37f2285f5f70a0c216adecc78c7e03
saliudev <liusa@utexas.edu>
2022-02-15 10:08:48 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -11,31 +11,10 @@ imgDenmark=loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/
 
 function setup( ){
     createCanvas(500, 500);
-    background(112,128,144);
-    textSize(22);
-    text('Type Denmark or Congo', 20, 40);
-    
-    input = createInput ( );
-    input.position( 20, 60);
-    input.input(showImage);
-    
-    botton = createButton('Submit');
-    botton.position(220, 60);
-    botton.mousePressed(showImage);
-    
-    console.log('I am in Setup');
+
 }
 
 function showImage( ){
     
-    if( input.value() == "Congo")
-   {
-    image( imgCongo, 20, 200, 400, 210);
-   }
 
-     else if( input.value() == "Denmark")
-   {
-    image( imgDenmark, 20, 200, 400, 210);
-    }
-       
    }
